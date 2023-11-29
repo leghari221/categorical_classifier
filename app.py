11d490658788ca0a4ee0d952835fc1618259aad7
Umais Leghari <leghari221@gmail.com>
2023-11-29 12:05:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import pickle
+import streamlit as st
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 # Load the trained model
 with open('naive_bayes_model.pkl', 'rb') as file:
@@ -8,25 +12,15 @@ with open('naive_bayes_model.pkl', 'rb') as file:
 with open('vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
-# Import necessary libraries
-import streamlit as st
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import metrics
-
 # Load the 20 Newsgroups dataset
-#newsgroups_train = fetch_20newsgroups(subset='train')
-#newsgroups_test = fetch_20newsgroups(subset='test')
+newsgroups_train = fetch_20newsgroups(subset='train')
+newsgroups_test = fetch_20newsgroups(subset='test')
 
 # Convert the text data into vectors
-#vectorizer = TfidfVectorizer()
-#vectors_train = vectorizer.fit_transform(newsgroups_train.data)
-#vectors_test = vectorizer.transform(newsgroups_test.data)
-
-# Train a Naive Bayes classifier
-#clf = MultinomialNB(alpha=.01)
-#clf.fit(vectors_train, newsgroups_train.target)
+# Note: If you have already preprocessed and saved vectors, you can load them instead
+# vectorizer = TfidfVectorizer()
+# vectors_train = vectorizer.fit_transform(newsgroups_train.data)
+# vectors_test = vectorizer.transform(newsgroups_test.data)
 
 # Streamlit app
 st.title('News Document Classification')
